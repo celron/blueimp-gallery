fd770a0531509af138f50bcafcf7df81c9eb0c2e
Allan Lee <erikos@Allans-Mac-Pro.local>
2016-05-15 18:40:13 -0400

    Updated to blueimp-gallery 2.21.2

--- vendor/assets/javascripts/jquery.blueimp-gallery.js
+++ vendor/assets/javascripts/jquery.blueimp-gallery.js
@@ -1,5 +1,5 @@
 /*
- * blueimp Gallery jQuery plugin 1.2.0
+ * blueimp Gallery jQuery plugin
  * https://github.com/blueimp/Gallery
  *
  * Copyright 2013, Sebastian Tschan
@@ -9,73 +9,75 @@
  * http://www.opensource.org/licenses/MIT
  */
 
-/*global define, window, document */
+/* global define, window, document */
 
-(function (factory) {
-    'use strict';
-    if (typeof define === 'function' && define.amd) {
-        define([
-            'jquery',
-            './blueimp-gallery.js'
-        ], factory);
-    } else {
-        factory(
-            window.jQuery,
-            window.blueimp.Gallery
-        );
-    }
+;(function (factory) {
+  'use strict'
+  if (typeof define === 'function' && define.amd) {
+    define([
+      'jquery',
+      './blueimp-gallery'
+    ], factory)
+  } else {
+    factory(
+      window.jQuery,
+      window.blueimp.Gallery
+    )
+  }
 }(function ($, Gallery) {
-    'use strict';
+  'use strict'
 
-    // Global click handler to open links with data-gallery attribute
-    // in the Gallery lightbox:
-    $(document).on('click', '[data-gallery]', function (event) {
-        // Get the container id from the data-gallery attribute:
-        var id = $(this).data('gallery'),
-            widget = $(id),
-            container = (widget.length && widget) ||
-                $(Gallery.prototype.options.container),
-            options = $.extend(
-                // Retrieve custom options from data-attributes
-                // on the Gallery widget:
-                container.data(),
-                {
-                    container: container[0],
-                    index: this,
-                    event: event,
-                    onopen: function () {
-                        container
-                            .data('gallery', this)
-                            .trigger('open');
-                    },
-                    onopened: function () {
-                        container.trigger('opened');
-                    },
-                    onslide: function () {
-                        container.trigger('slide', arguments);
-                    },
-                    onslideend: function () {
-                        container.trigger('slideend', arguments);
-                    },
-                    onslidecomplete: function () {
-                        container.trigger('slidecomplete', arguments);
-                    },
-                    onclose: function () {
-                        container.trigger('close');
-                    },
-                    onclosed: function () {
-                        container
-                            .trigger('closed')
-                            .removeData('gallery');
-                    }
-                }
-            ),
-            // Select all links with the same data-gallery attribute:
-            links = $('[data-gallery="' + id + '"]');
-        if (options.filter) {
-            links = links.filter(options.filter);
-        }
-        return new Gallery(links, options);
-    });
-
-}));
+  // Global click handler to open links with data-gallery attribute
+  // in the Gallery lightbox:
+  $(document).on('click', '[data-gallery]', function (event) {
+    // Get the container id from the data-gallery attribute:
+    var id = $(this).data('gallery')
+    var widget = $(id)
+    var container = (widget.length && widget) ||
+          $(Gallery.prototype.options.container)
+    var callbacks = {
+      onopen: function () {
+        container
+          .data('gallery', this)
+          .trigger('open')
+      },
+      onopened: function () {
+        container.trigger('opened')
+      },
+      onslide: function () {
+        container.trigger('slide', arguments)
+      },
+      onslideend: function () {
+        container.trigger('slideend', arguments)
+      },
+      onslidecomplete: function () {
+        container.trigger('slidecomplete', arguments)
+      },
+      onclose: function () {
+        container.trigger('close')
+      },
+      onclosed: function () {
+        container
+          .trigger('closed')
+          .removeData('gallery')
+      }
+    }
+    var options = $.extend(
+      // Retrieve custom options from data-attributes
+      // on the Gallery widget:
+      container.data(),
+      {
+        container: container[0],
+        index: this,
+        event: event
+      },
+      callbacks
+    )
+    // Select all links with the same data-gallery attribute:
+    var links = $('[data-gallery="' + id + '"]')
+    if (options.filter) {
+      links = links.filter(options.filter)
+    }
+    return new Gallery(links, options)
+  })
+}))
